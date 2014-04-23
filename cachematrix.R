f594e61e2297f36751bf706b8429f29152319c61
KeiFy <qifengyi@usc.edu>
2014-04-23 11:20:16 -0500

    assignment answer

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- matrix(NA,nrow(x),ncol(x))
+        set <- function(y){
+                x<<-y
+                s<<-matrix(NA,nrow(x),ncol(x))             
+        }
+        get <- function()x
+        setinverse <- function(solve) s<<-solve
+        getinverse <- function() s
+        list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 
@@ -12,4 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        s <- x$getinverse()
+        if(!is.na(s[1,1])){
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data,...)
+        x$setinverse(s)
+        s
 }
